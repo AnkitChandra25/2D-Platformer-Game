cff9f236c47fb42583671a556605985713e7a2b7
Ankit <ankitchandra25sept@gmail.com>
2025-05-04 20:06:08 +0530

    Player Controller

---
+++ Assets/Game Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Awake()
+    {
+        Debug.Log("Player_Controller Awake");
+    }
+
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed)); 
+
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+}
